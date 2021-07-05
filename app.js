26bb79b3b758c48258c362850e98ac42ccd74953
J.I Moreno <joseignacio.martinmoreno@gmail.com>
2021-07-05 19:06:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,4 +35,9 @@ app.use('/api/greeting', (request, response) => {
   response.send({"data":[{"value": "DirectCostsId","b2x": "return \"Direct costs\";","verbalization": "Direct costs"},{"value": "IndirectCostsId","b2x": "return \"Indirect costs\";","verbalization": "Indirect costs"},{"value": "Other","b2x": "return \"Other\";","verbalization": "Other"}]});
 });
 
+app.use('/api/scopeOrCategory', (request, response) => {
+  const name = request.query ? request.query.name : undefined;
+  response.send({"data":[{"value": "DirectCostsId","b2x": "return \"Direct costs\";","verbalization": "Direct costs"},{"value": "IndirectCostsId","b2x": "return \"Indirect costs\";","verbalization": "Indirect costs"},{"value": "Other","b2x": "return \"Other\";","verbalization": "Other"}]});
+});
+
 module.exports = app;
